ccb27649f90641ba9408fcd2a0cf3b95cc42f080
YukiWatanabe824 <gyggg950@gmail.com>
2022-11-27 15:16:12 +0900

    comment paramsの項目を変更
    content以外はparamsから取得しないため削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -36,6 +36,6 @@ class CommentsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:user_id, :content, :commentable_type, :commentable_id)
+    params.require(:comment).permit(:content)
   end
 end
