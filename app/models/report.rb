150de0e1a1ae68723d540856588a2298ee762b80
YukiWatanabe824 <gyggg950@gmail.com>
2022-11-27 15:57:22 +0900

    userに関するvaridationを削除
    belongs_toの段階でuser必須となるため

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,6 @@
 
 class Report < ApplicationRecord
   belongs_to :user
-  validates :user_id, presence: true
   validates :content, presence: true
   validates :title, presence: true
   has_many :comments, as: :commentable, dependent: :destroy
